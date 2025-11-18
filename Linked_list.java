987b9fd3b2e347783822569916ef170353ecad17
awadi99 <adityawaghmare9990@gmail.com>
2025-11-19 03:49:16 +0530

    LinkList Node

--- Linked_list.java
+++ Linked_list.java
@@ -1,19 +1,22 @@
 public class Linked_list {
 
-    public static class Node{
-        int data;
+    public static class Node {
+        int data ;
         Node next;
 
         public Node(int data){
             this.data=data;
             this.next=null;
         }
-    }
 
-    public static Node head;
-    public static Node taill;
+    }
+        public static Node head;
+        public static Node tail;
+    
     public static void main(String args[]){
         Linked_list ll = new Linked_list();
+        ll.head = new Node(1);
+        ll.head.next=new Node(2);
 
     }
 }
