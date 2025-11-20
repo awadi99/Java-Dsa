fb27f9ef893cd3df14fae842c22295143ac3f350
awadi99 <adityawaghmare9990@gmail.com>
2025-11-21 02:15:34 +0530

    Next Permutation

--- leetcode.java
+++ leetcode.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 public class leetcode {
 
     // question : 26. Remove Duplicates from Sorted Array
@@ -218,6 +219,41 @@ public class leetcode {
         return result;
     }
 
+    public static void Next_Permutation(int nums[]) {
+        int n = nums.length;
+        int gola = -1;
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                gola = i - 1;
+                break;
+            }
+        }
+
+        if (gola != -1) {
+            int swap = gola;
+            for (int j = n - 1; j >= gola + 1; j--) {
+                if (nums[j] > nums[gola]) {
+                    swap = j;
+                    break;
+                }
+            }
+            int temp = nums[gola];
+            nums[gola] = nums[swap];
+            nums[swap] = temp;
+        }
+        Next_Permutation_Reverse(nums, gola + 1, n - 1);
+    }
+
+    public static void Next_Permutation_Reverse(int nums[], int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
     public static void main(String[] args) {
         // question : 26. Remove Duplicates from Sorted Array
         /*
@@ -372,6 +408,52 @@ public class leetcode {
         int elements[] = { 3, 5, 2, 3 };
         // normal way they give 8 maximum number and optimize way they give 7 maximum
         System.err.println("Answer = " + minPairSum(elements));
+
+        // 31 Next Permutation
+        /*
+         * A permutation of an array of integers is an arrangement of its members into a
+         * sequence or linear order.
+         * 
+         * For example, for arr = [1,2,3], the following are all the permutations of
+         * arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+         * The next permutation of an array of integers is the next lexicographically
+         * greater permutation of its integer. More formally, if all the permutations of
+         * the array are sorted in one container according to their lexicographical
+         * order, then the next permutation of that array is the permutation that
+         * follows it in the sorted container. If such arrangement is not possible, the
+         * array must be rearranged as the lowest possible order (i.e., sorted in
+         * ascending order).
+         * 
+         * For example, the next permutation of arr = [1,2,3] is [1,3,2].
+         * Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+         * While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does
+         * not have a lexicographical larger rearrangement.
+         * Given an array of integers nums, find the next permutation of nums.
+         * 
+         * The replacement must be in place and use only constant extra memory.
+         * 
+         * 
+         * Example 1:
+         * 
+         * Input: nums = [1,2,3]
+         * Output: [1,3,2]
+         * Example 2:
+         * 
+         * Input: nums = [3,2,1]
+         * Output: [1,2,3]
+         * Example 3:
+         * 
+         * Input: nums = [1,1,5]
+         * Output: [1,5,1]
+         */
+
+        int Permutation[]={1,3,4,5,2};
+        Next_Permutation(Permutation);
+        for (int ele : Permutation)
+        {
+            System.err.print(" "+ele);
+        }
+
 
     }
 }
