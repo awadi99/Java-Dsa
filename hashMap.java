41575dd2bdeab7e0ffc6182b0dc2228268ef117b
awadi99 <adityawaghmare9990@gmail.com>
2026-01-31 22:50:44 +0530

    HashMap

---
+++ hashMap.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class hashMap{
+
+
+    public static void main(String[] args) {
+        HashMap<String , Integer> map = new HashMap<>();
+
+        map.put("USA",1);
+        map.put("RUSSIA",2);
+        map.put("China",3);
+        map.put("India",4);
+        map.put("UK",5);
+
+        System.err.println("HASH-MAP = " + map);
+
+        map.remove("India");
+        System.err.println(map);
+
+
+        System.err.println("SIZE OF HASHMAP = "+map.size());
+        map.clear();
+        System.err.println("CHECK EMPTY = "+map.isEmpty());
+    }
+}
