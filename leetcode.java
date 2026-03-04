6a59b5de7ea855b3253a7d4d6f82c7060cfd6906
awadi99 <adityawaghmare9990@gmail.com>
2026-03-04 16:33:46 +0530

    romanToInt

--- leetcode.java
+++ leetcode.java
@@ -555,9 +555,9 @@ public class leetcode {
 
     // 5. Longest Palindromic Substring
 
-    public static boolean solve(String s, int i ,int j){
-        while(i < j){
-            if(s.charAt(i)!=s.charAt(j)){
+    public static boolean solve(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
             i++;
@@ -566,22 +566,55 @@ public class leetcode {
         return true;
     }
 
-    public static String longestPalindrome(String s){
+    public static String longestPalindrome(String s) {
         int n = s.length();
         int maxLen = Integer.MIN_VALUE;
-        int sp = 0 ;
-        for(int i = 0 ; i < n ; i ++){
-            for(int j = i ; j < n; j++){
-                if(solve(s, i, j)==true){
-                    if(j - i  + 1 > maxLen){
-                        maxLen =  j-i+1;
+        int sp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j) == true) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
                         sp = i;
                     }
                 }
             }
         }
-        return s.substring(sp,sp+maxLen);
+        return s.substring(sp, sp + maxLen);
     }
+
+
+    // 13. Roman to Integer
+
+    public static int getValue(char s){
+        switch(s){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
+        }
+    }
+
+    public static int romanToInt(String s){
+        int n = s.length();
+        int sum  = 0;
+        
+        for(int i = 0 ; i < n; i++){
+            char ch = s.charAt(i);
+            if((i+1) < n  && getValue(ch)<getValue(s.charAt(i+1))){
+                sum=sum-getValue(ch);
+            }
+            else{
+                sum += getValue(ch);
+            }
+        }
+        return sum;
+    }
+
 
 
     public static void main(String[] args) {
@@ -1223,8 +1256,68 @@ public class leetcode {
          * s consist of only digits and English letters.
          */
 
-
         String Input = "babad";
-        System.err.println("longestPalindrome =  "+longestPalindrome(Input));
+        System.err.println("longestPalindrome =  " + longestPalindrome(Input));
+
+        // 13. Roman to Integer
+        /*
+         * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+         * and M.
+         * 
+         * Symbol Value
+         * I 1
+         * V 5
+         * X 10
+         * L 50
+         * C 100
+         * D 500
+         * M 1000
+         * For example, 2 is written as II in Roman numeral, just two ones added
+         * together. 12 is written as XII, which is simply X + II. The number 27 is
+         * written as XXVII, which is XX + V + II.
+         * 
+         * Roman numerals are usually written largest to smallest from left to right.
+         * However, the numeral for four is not IIII. Instead, the number four is
+         * written as IV. Because the one is before the five we subtract it making four.
+         * The same principle applies to the number nine, which is written as IX. There
+         * are six instances where subtraction is used:
+         * 
+         * I can be placed before V (5) and X (10) to make 4 and 9.
+         * X can be placed before L (50) and C (100) to make 40 and 90.
+         * C can be placed before D (500) and M (1000) to make 400 and 900.
+         * Given a roman numeral, convert it to an integer.
+         * 
+         * 
+         * 
+         * Example 1:
+         * 
+         * Input: s = "III"
+         * Output: 3
+         * Explanation: III = 3.
+         * Example 2:
+         * 
+         * Input: s = "LVIII"
+         * Output: 58
+         * Explanation: L = 50, V= 5, III = 3.
+         * Example 3:
+         * 
+         * Input: s = "MCMXCIV"
+         * Output: 1994
+         * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+         * 
+         * 
+         * Constraints:
+         * 
+         * 1 <= s.length <= 15
+         * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+         * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+         * 
+         */
+
+        String Roman = "MCMXCIV";
+        System.err.println(" Roman answer  = " +romanToInt(Roman));
+
+
+
     }
 }
