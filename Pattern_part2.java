c413cf08b130f4a95be783c3293c89d6d90aaef6
awadi99 <adityawaghmare9990@gmail.com>
2026-06-23 22:05:05 +0530

    Patten Hollow

--- Pattern_part2.java
+++ Pattern_part2.java
@@ -18,6 +18,20 @@ public class Pattern_part2 {
     }
 
 
+    public static void patternHollow(int row , int col){
+        for (int i =1; i <=row; i++){
+            for (int j =1;j<=col;j++){
+                if(i==1||i==row||j==1||j==col){
+                    System.err.print(" *");
+                }else{
+                    System.err.print("  ");
+                }
+            }
+                        System.err.println();
+        }
+    }
+
+
     public static void Inverted_pattern_Rotated(int n){
         // n means = row
         for(int i = 1;i<=n;i++)
@@ -218,6 +232,11 @@ public class Pattern_part2 {
         // Is_Diamond_Pattern(3);
 
         // Is_turn_Box(4 );
+
+        
+
+
+
     }
 
 }
